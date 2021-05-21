1c75c44b7737da84c3ad6ab39eda5fc273616b6a
Jens Gerke <jens.gerke@vorteil.io>
2021-05-22 07:42:52 +1000

    first
    Signed-off-by: Jens Gerke <jens.gerke@vorteil.io>

--- app/app.go
+++ app/app.go
@@ -6,22 +6,46 @@ import (
 	"github.com/sethvargo/go-githubactions"
 )
 
+type args struct {
+	name  string
+	value string
+}
+
+const (
+	serverIdx   = iota
+	workflowIdx = iota
+	tokenIdx    = iota
+	dataIdx     = iota
+)
+
 func main() {
 
-	var server string
-	getValue(&server, "server")
+	in := []args{
+		args{
+			name: "server",
+		},
+		args{
+			name: "workflow",
+		},
+		args{
+			name: "token",
+		},
+		args{
+			name: "data",
+		},
+	}
 
-	fmt.Printf("using server: %v", server)
+	for _, i := range in {
+		getValue(&i.value, i.name)
+	}
+
+	fmt.Printf("using server: %v", in[serverIdx])
+	fmt.Printf("executing workflow: %v", in[workflowIdx])
 
 	// fmt.Printf("ARGS %v\n", os.Args)
 	// fmt.Printf("ENVS %v\n", os.Environ())
 }
 
 func getValue(val *string, key string) {
-
 	*val = githubactions.GetInput(key)
-	if *val == "" {
-		githubactions.Fatalf("missing '%s'", key)
-	}
-
 }
