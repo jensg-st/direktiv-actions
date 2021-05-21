39b8683d6025e2895f645603aef630bcfb3a913f
Jens Gerke <jens.gerke@vorteil.io>
2021-05-22 07:29:31 +1000

    first
    Signed-off-by: Jens Gerke <jens.gerke@vorteil.io>

--- app/app.go
+++ app/app.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
-	"os"
+
+	"github.com/sethvargo/go-githubactions"
 )
 
 func main() {
-	fmt.Printf("ARGS %v\n", os.Args)
-	fmt.Printf("ENVS %v\n", os.Environ())
+
+	var server string
+	getValue(&server, "server")
+
+	fmt.Printf("using server: %v", server)
+
+	// fmt.Printf("ARGS %v\n", os.Args)
+	// fmt.Printf("ENVS %v\n", os.Environ())
+}
+
+func getValue(val *string, key string) {
+
+	*val = githubactions.GetInput(key)
+	if *val == "" {
+		githubactions.Fatalf("missing '%s'", key)
+	}
+
 }
